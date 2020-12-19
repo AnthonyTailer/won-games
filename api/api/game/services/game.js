a0441bbcb7ec8ddce21496585f5864d7941d6aad
AnthonyTailer <aalmeida@inf.ufsm.br>
2020-12-19 17:14:50 -0300

    feat: add createGames  scrapter function

--- api/api/game/services/game.js
+++ api/api/game/services/game.js
@@ -34,7 +34,7 @@ async function getGameInfo(slug) {
 }
 
 async function getByName(name, entityName) {
-  const item = strapi.services[entityName].find({ name });
+  const item = await strapi.services[entityName].find({ name });
   return item.length ? item[0] : null;
 }
 
@@ -78,6 +78,38 @@ async function createManyToManyData(products) {
   ]);
 }
 
+
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game")
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`)
+
+        const game =  await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"), // just ajust slug
+          price: product.price.amount,
+          release_date: new Date(Number(product.globalReleaseDate * 1000)).toISOString(),
+          categories: await Promise.all(
+            product.genres.map(name => getByName(name, 'category'))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map(name => getByName(name, 'platform'))
+          ),
+          developers: [await getByName(product.developer, 'developer')],
+          publisher: await getByName(product.publisher, 'publisher'),
+          ...(await getGameInfo(product.slug))
+        })
+
+        return game
+      }
+
+    })
+  )
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `${BASE_URL}/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
@@ -85,5 +117,6 @@ module.exports = {
     const { data: { products } } = await axios.get(gogApiUrl)
     // console.log(await getGameInfo(slug))
     await createManyToManyData(products)
+    await createGames(products)
   }
 };
