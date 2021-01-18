d554e6b750521ac1459305c7c6d124d417217859
AnthonyTailer <aalmeida@inf.ufsm.br>
2021-01-18 18:16:06 -0300

    feat: add Menu component

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -4,6 +4,19 @@ import theme from 'styles/theme'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white
+      },
+      {
+        name: 'dark',
+        value: theme.colors.black
+      }
+    ]
+  }
 }
 
 export const decorators = [
