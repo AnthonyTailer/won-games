1bd39ca2e73c8834a399d602157f959371797506
Anthony Tailer <anthony.almeida@dbccompany.com.br>
2021-12-30 17:02:37 -0300

    refactor: add Form component styles

--- client/src/components/Form/index.tsx
+++ client/src/components/Form/index.tsx
@@ -4,11 +4,12 @@ import { darken } from 'polished'
 import * as TextFieldStyles from 'components/TextField/styles'
 import * as ButtonStyles from 'components/Button/styles'
 
-export const Wrapper = styled.div`
+export const FormWrapper = styled.div`
   ${({ theme }) => css`
     ${TextFieldStyles.Wrapper} {
       margin: ${theme.spacings.xxsmall} 0;
     }
+
     ${ButtonStyles.Wrapper} {
       margin: ${theme.spacings.medium} auto ${theme.spacings.xsmall};
     }
